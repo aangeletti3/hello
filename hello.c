3b0f897c1841c3f0547e4bbc7edb7626d126ee42
Anton Angeletti <aangeletti3@huskers.unl.edu>
2022-08-25 16:35:17 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Anton Angeletti
+ *         aangeletti3@huskers.unl.edu
+ * Date: 2022/08/25
+ *
+ * A C program to print my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  printf("Anton Angeletti\n");
+  return 0;
+}
